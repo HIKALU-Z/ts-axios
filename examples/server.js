bf27987ecd8a98541531dc9d7f30117c4c791119
HIKALU <play3a@126.com>
2019-08-06 17:26:19 +0800

    feat:add url handle

--- examples/server.js
+++ examples/server.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9000
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
@@ -36,6 +36,10 @@ router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
   })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
 })
 
 app.use(router)
---
+++ src/helpers/url.ts
@@ -0,0 +1,62 @@
+import { isDate, isObject } from './util'
+
+/**
+ * 加密 url 使得 url 符合规范
+ * 但是对于部分特殊字符不做转换 例如 @ : 等
+ *
+ * @param {string} val
+ * @returns {string}
+ */
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function bulidURL(url: string, params?: any) {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    let val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values: string[]
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
