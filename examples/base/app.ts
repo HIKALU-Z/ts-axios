8ab88b6601788335b784ffc3ff31db76d934dc42
HIKALU <play3a@126.com>
2019-08-07 16:19:38 +0800

    feat: add header handle
    before send add header for xhr

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -77,3 +77,36 @@ axios({
   url: '/base/buffer',
   data: arr
 })
+
+
+// header
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
